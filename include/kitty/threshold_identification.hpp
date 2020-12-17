f40aa2c83a22810a6ebf7104afdf12b2b6314dff
CarloCaputoEPFL <75539254+CarloCaputoEPFL@users.noreply.github.com>
2020-12-17 19:01:22 +0100

    v2

--- include/kitty/threshold_identification.hpp
+++ include/kitty/threshold_identification.hpp
@@ -1,3 +1,4 @@
+
 /* kitty: C++ truth table library
  * Copyright (C) 2017-2020  EPFL
  *
@@ -48,10 +49,10 @@ namespace kitty
   where w_i are the weight values and T is the threshold value.
   The linear form of a TF is the vector [w_1, ..., w_n; T].
   \param tt The truth table
-  \param plf Pointer to a vector that will hold a linear form of `tt` if it is a TF.
-             The linear form has `tt.num_vars()` weight values and the threshold value
+  \param plf Pointer to a vector that will hold a linear form of tt if it is a TF.
+             The linear form has tt.num_vars() weight values and the threshold value
              in the end.
-  \return `true` if `tt` is a TF; `false` if `tt` is a non-TF.
+  \return true if tt is a TF; false if tt is a non-TF.
 */
 template<typename TT, typename = std::enable_if_t<is_complete_truth_table<TT>::value>>
 bool is_threshold( const TT& tt, std::vector<int64_t>* plf = nullptr )
@@ -66,42 +67,38 @@ bool is_threshold( const TT& tt, std::vector<int64_t>* plf = nullptr )
   {
     //controllo la funzione
 
-      // auto num_vars= tt.num_vars();
-      bool su=false, giu=false;
+    // auto num_vars= tt.num_vars();
+    bool su = false, giu = false;
 
-      auto cof0 = cofactor0(tt,i);
-      auto cof1 = cofactor1(tt,i);
+    auto cof0 = cofactor0( tt, i );
+    auto cof1 = cofactor1( tt, i );
 
-      uint32_t Num = tt.num_bits();
+    uint32_t Num = tt.num_bits();
 
-      for (auto j=0u; j<Num ; ++j){
-        if(get_bit(cof1,j) > get_bit(cof0,j))
-          su=true;
-        if(get_bit(cof1,j) < get_bit(cof0,j))
-          giu=true;
-      }
-      //binate
-      if (su && giu)
-        return false;
-
-      //negative
-      if (giu && !su)
-      { //negative
-        var_inv[i] = true;
-        mytt = flip( mytt, i );
-      //  std::cout << "x" << i+1 << "neg_un" << std::endl;
-
-      }
-
-
+    for ( auto j = 0u; j < Num; ++j )
+    {
+      if ( get_bit( cof1, j ) > get_bit( cof0, j ) )
+        su = true;
+      if ( get_bit( cof1, j ) < get_bit( cof0, j ) )
+        giu = true;
     }
+    //binate
+    if ( su && giu )
+      return false;
 
-
+    //negative
+    if ( giu && !su )
+    { //negative
+      var_inv[i] = true;
+      mytt = flip( mytt, i );
+      //  std::cout << "x" << i+1 << "neg_un" << std::endl;
+    }
+  }
 
   //creating lp
 
-  lprec* lp=NULL;
-  int Ncol, *colno = NULL, j, sol=0;
+  lprec* lp = NULL;
+  int Ncol, *colno = NULL, j, sol = 0;
   REAL* row = NULL;
   Ncol = num_vars + 1; //+1 to invlude T
   const int T = Ncol;
@@ -113,79 +110,74 @@ bool is_threshold( const TT& tt, std::vector<int64_t>* plf = nullptr )
   }
 
   /* create space large enough for one row */
-  colno = (int*)malloc( Ncol * sizeof( colno ) );
-  row = (REAL*)malloc( Ncol * sizeof( row ) );
+  colno = (int*)malloc( Ncol * sizeof( int ) );
+  row = (REAL*)malloc( Ncol * sizeof( *row ) );
 
   if ( ( colno == NULL ) || ( row == NULL ) )
     return false;
 
   set_add_rowmode( lp, TRUE ); /* makes building the model faster if it is done rows by row */
-if ( true)
+
+  //I take on and off set
+  std::vector<cube> on_set = isop( mytt );
+  std::vector<cube> off_set = isop( unary_not( mytt ) );
+//Constraints for on_set
+  for ( cube i : on_set )
   {
-    //I take on and off set
-    std::vector<cube> on_set = get_prime_implicants_morreale( mytt );
-    std::vector<cube> off_set =  get_prime_implicants_morreale ( unary_not( mytt ) );
-
-    //Constraints for on_set
-    for ( cube i : on_set )
+    j = 0;
+    for ( int k = 0; k < num_vars; k++ )
     {
-      j = 0;
-      for ( int k = 0; k < num_vars; k++ )
+      //I control if the variable is present in the cube
+      if ( i.get_mask( k ) == 1 )
       {
-        //I control if the variable is present in the cube
-        if ( i.get_mask( k ) == 1 )
-        {
-          colno[j] = k + 1;
-          row[j] = 1;
-          j++;
-        }
+        colno[j] = k + 1;
+        row[j] = 1;
+        j++;
       }
-      colno[j] = T;
-      row[j] = -1;
-      j++;
-      add_constraintex( lp, j, row, colno, GE, 0.0 );
     }
-
-    //constraints for offset
-    for ( cube i : off_set )
-    {
-      j = 0;
-      for ( int k = 0; k < num_vars; k++ )
-      {
-        if ( i.get_mask( k ) == 0 ) //variable not in the cube
-        {
-          colno[j] = k + 1;
-          row[j] = 1;
-          j++;
-        }
-      }
-      colno[j] = T;
-      row[j] = -1;
-      j++;
-      add_constraintex( lp, j, row, colno, LE, -1.0 );
-    }
+    colno[j] = T;
+    row[j] = -1;
+    j++;
+    add_constraintex( lp, j, row, colno, GE, 0 );
   }
+
+  //constraints for offset
+  for ( cube i : off_set )
+  {
+    j = 0;
+    for ( int k = 0; k < num_vars; k++ )
+    {
+      if ( i.get_mask( k ) == 0 ) //variable not in the cube
+      {
+        colno[j] = k + 1;
+        row[j] = 1;
+        j++;
+      }
+    }
+    colno[j] = T;
+    row[j] = -1;
+    j++;
+    add_constraintex( lp, j, row, colno, LE, -1 );
+  }
+
   //Make variables integer
-   for ( int i = 1; i < Ncol+1; i++ )
-  {
-    set_int(lp,i,TRUE);
-  }
-
+  // for ( int i = 1; i < Ncol + 1; i++ )
+  // {
+  //   set_int( lp, i, TRUE );
+  // }
 
   //all greater than 0
-  /*
-  for ( int i = 1; i < num_vars + 2; i++ )
-  {
-    for ( int l = 0; l< num_vars+2 ; l++){
-      row[l]=0.0;
-    }
-    row[i]=1.0;
-    add_constraint(lp,row,GE,0.0);
-  }
-   */
-  set_add_rowmode( lp, FALSE );
- // print_lp(lp);
 
+  // for ( int i = 1; i < num_vars + 2; i++ )
+  // {
+  //   for ( int l = 0; l< num_vars+2 ; l++){
+  //     row[l]=0.0;
+  //   }
+  //   row[i]=1.0;
+  //   add_constraint(lp,row,GE,0.0);
+  // }
+
+  set_add_rowmode( lp, FALSE );
 
   //sum of all variables plus T
   /* set the objective function */
@@ -198,46 +190,38 @@ if ( true)
   }
   set_obj_fnex( lp, Ncol, row, colno );
 
-
-
   /* set the object direction to ìminimze */
   set_minim( lp );
   set_verbose( lp, IMPORTANT );
 
- // print_lp(lp);
+  print_lp( lp );
 
-  sol=solve(lp);
+  sol = solve( lp );
   //if it is not a TF return out
   if ( sol != OPTIMAL )
     return false;
 
   //get variable values
 
-  get_variables(lp,row);
-
-/* free allocated memory */
-  if(row != NULL)
-   free(row);
-  if(colno != NULL)
-    free(colno);
-
-// if(lp != NULL) {
-    /* clean up such that all used memory by lpsolve is freed */
+  get_variables( lp, row );
+  // if(lp != NULL) {
+  /* clean up such that all used memory by lpsolve is freed */
   //  delete_lp(lp);
- // }
-
-  delete_lp(lp);
+  // }
 
   //forming the linear form
 
-  for (int i=0; i<Ncol; i++){
-    linear_form.emplace_back((int64_t)row[i]);
+  for ( int i = 0; i < Ncol; i++ )
+  {
+    linear_form.push_back( row[i] );
   }
   //change negated variables
-  for (int i=0; i<num_vars; i++){
-    if (var_inv[i]==true){
-      linear_form[i]=-linear_form[i];
-      linear_form[Ncol-1]=linear_form[Ncol-1]+linear_form[i];
+  for ( int i = 0; i < num_vars; i++ )
+  {
+    if ( var_inv[i] == true )
+    {
+      linear_form[i] = -linear_form[i];
+      linear_form[Ncol - 1] = linear_form[Ncol - 1] + linear_form[i];
     }
   }
 
@@ -246,8 +230,14 @@ if ( true)
     *plf = linear_form;
   }
 
+  /* free allocated memory */
+  if ( row != NULL )
+    free( row );
+  if ( colno != NULL )
+    free( colno );
+  delete_lp( lp );
+
   return true;
 }
-
 
 } /* namespace kitty */
